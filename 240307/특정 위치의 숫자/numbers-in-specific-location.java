c1894238415265a46abb8d02566a4cbc7488a4d7
ticketpaper <122894395+ticketpaper@users.noreply.github.com>
2024-03-07 23:02:53 +0900

    Title: 특정 위치의 숫자, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240307/특정 위치의 숫자/numbers-in-specific-location.java
+++ 240307/특정 위치의 숫자/numbers-in-specific-location.java
@@ -2,9 +2,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
         for(int i=0; i<10; i++){
-            int a = sc.nextInt();
-            if()
+            arr[i] = sc.nextInt();
         }
+        System.out.println(arr[2]+arr[4]+arr[9]);
     }
 }
